f855bf3e0cee72fb83aa672554e4d52157aa8a8a
Denys G. Santos <gsdenys@gmail.com>
2023-10-23 23:28:04 -0400

    add comments

--- config.go
+++ config.go
@@ -1,11 +1,15 @@
+// Package gormet provides functionality for working with GORM ORM.
 package gormet
 
+// Config represents the configuration options for the gormet package.
 type Config struct {
+	// Validate indicates whether entity validation is enabled or not.
 	Validate bool
 }
 
+// DefaultConfig returns a pointer to a Config with default values.
 func DefaultConfig() *Config {
 	return &Config{
-		Validate: true,
+		Validate: true, // Entity validation is enabled by default.
 	}
 }
--- repository.go
+++ repository.go
@@ -1,3 +1,4 @@
+// Package gormet provides generic repository functionality for GORM ORM with validation support.
 package gormet
 
 import (
@@ -7,14 +8,16 @@ import (
 	"gorm.io/gorm"
 )
 
+// Repository represents a generic repository for GORM ORM.
 type Repository[T any] struct {
-	DB     *gorm.DB
-	Config *Config
+	DB     *gorm.DB // DB represents the GORM database connection.
+	Config *Config  // Config holds the configuration settings for the repository.
 }
 
+// CreateRepository creates a new repository with the given database connection and configuration.
 func CreateRepository[T any](db *gorm.DB, conf *Config) *Repository[T] {
 	if conf == nil {
-		conf = DefaultConfig()
+		conf = DefaultConfig() // Set default configuration if not provided.
 	}
 
 	return &Repository[T]{
@@ -23,19 +26,23 @@ func CreateRepository[T any](db *gorm.DB, conf *Config) *Repository[T] {
 	}
 }
 
+// New creates and returns a new repository with the given database connection.
 func New[T any](db *gorm.DB) *Repository[T] {
 	return (*Repository[T])(CreateRepository[T](db, nil))
 }
 
+// Create inserts a new entity into the database using the repository.
 func (r *Repository[T]) Create(entity *T) error {
 	if entity == nil {
 		return errors.New("the entity cannot be nil")
 	}
 
+	// Validate the entity if validation is enabled in the configuration.
 	if err := isValid[T](entity, r.Config.Validate); err != nil {
 		return err
 	}
 
+	// Create the entity in the database.
 	if result := r.DB.Create(entity); result.Error != nil {
 		return result.Error
 	}
@@ -43,6 +50,7 @@ func (r *Repository[T]) Create(entity *T) error {
 	return nil
 }
 
+// isValid checks the validity of the entity based on the validation configuration.
 func isValid[T any](entity *T, validable bool) error {
 	if validable {
 		validate := validator.New()

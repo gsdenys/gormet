89240e0940dc5d7cd35fd10588b353b55deed5da
Denys G. Santos <gsdenys@gmail.com>
2023-11-11 21:00:57 -0500

    Create Delete and DeleteById functions

--- delete.go
+++ delete.go
@@ -32,7 +32,7 @@ func (r *Repository[T]) DeleteById(id interface{}) error {
 	}
 
 	condition := fmt.Sprintf("%s = ?", r.pkName)
-	deleteResult := r.db.Debug().Delete(new(T), condition, id)
+	deleteResult := r.db.Delete(new(T), condition, id)
 
 	if deleteResult.Error != nil {
 		return deleteResult.Error
@@ -42,5 +42,50 @@ func (r *Repository[T]) DeleteById(id interface{}) error {
 		return fmt.Errorf("no register found")
 	}
 
+	return nil
+}
+
+// Delete removes an entity from the database.
+//
+// This method takes an entity as an argument, ensures it is not nil, and then uses GORM's Delete method
+// to delete the corresponding record from the database. If the operation is successful, it returns nil.
+// If the operation fails, an error is returned, which could be due to database connectivity issues or other constraints.
+//
+// Usage:
+// err := repo.Delete(&entity)
+//
+//	if err != nil {
+//	    // Handle error
+//	}
+//
+// Parameters:
+//   - entity: A pointer to an instance of type T that represents the entity to be deleted from the database.
+//     It should not be nil.
+//
+// Returns:
+// - nil if the entity is successfully deleted from the database.
+// - An error if the entity is nil or if GORM encounters any issues while deleting the record.
+func (r *Repository[T]) Delete(entity *T) error {
+	// Validate that the entity is not nil to prevent potential issues.
+	if entity == nil {
+		// Return an error indicating that the entity must not be nil.
+		return errors.New("the entity should not be nil")
+	}
+
+	// Use GORM's Delete method to remove the record from the database.
+	// Debug mode is enabled for more detailed logs during development.
+	deleteResult := r.db.Debug().Delete(entity)
+
+	// Check if the Delete operation encountered an error.
+	if deleteResult.Error != nil {
+		// Return the encountered error.
+		return deleteResult.Error
+	}
+
+	if deleteResult.RowsAffected == 0 {
+		return fmt.Errorf("no register found")
+	}
+
+	// Return nil, indicating successful deletion.
 	return nil
 }
--- delete_test.go
+++ delete_test.go
@@ -84,3 +84,98 @@ func TestRepository_DeleteById(t *testing.T) {
 		assert.Equal(t, err.Error(), "sql: database is closed")
 	})
 }
+
+func TestRepository_Delete(t *testing.T) {
+
+	type testDelete struct {
+		gorm.Model
+		Email string `json:"email" gorm:"unique,not null;default:null"`
+		Name  string `json:"name" gorm:"unique;not null;default:null"`
+	}
+
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+
+	db.AutoMigrate(&testDelete{})
+
+	repo, err := New[testDelete](db)
+	assert.Nil(t, err)
+
+	createRegister := func() *testDelete {
+		var field = uuid.NewString()
+		entity := &testDelete{
+			Name:  fmt.Sprintf("%v", field),
+			Email: fmt.Sprintf("%v@mail.com", field),
+		}
+
+		repo.Create(entity)
+
+		return entity
+	}
+
+	t.Run("Updated Successfull", func(t *testing.T) {
+		entity := createRegister()
+		id := entity.ID
+
+		err := repo.Delete(entity)
+		assert.Nil(t, err)
+
+		var got *testDelete = &testDelete{}
+		tx := db.First(got, "id = ?", id)
+
+		assert.NotNil(t, tx)
+		assert.NotNil(t, tx.Error)
+		assert.Equal(t, "record not found", tx.Error.Error())
+		assert.Equal(t, uint(0), got.ID)
+	})
+
+	t.Run("Nil entity", func(t *testing.T) {
+		err := repo.Delete(nil)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "the entity should not be nil")
+	})
+
+	t.Run("Entity with sql injection", func(t *testing.T) {
+		entity := createRegister()
+
+		name := fmt.Sprintf(`%s"; update test_deletes set deleted at '1970-01-01 00:00:00 --`, entity.Name)
+		entity.Name = name
+
+		err = repo.Delete(entity)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Entity with sql injection in string ID", func(t *testing.T) {
+		type testDeletePkString struct {
+			Email string `json:"email" gorm:"primaryKey;default:null"`
+			Name  string `json:"name" gorm:"default:null"`
+		}
+
+		db.AutoMigrate(&testDeletePkString{})
+
+		repo, err := New[testDeletePkString](db)
+		assert.Nil(t, err)
+
+		entity := &testDeletePkString{}
+		name := fmt.Sprintf(`%s"; update test_deletes set deleted at '1970-01-01 00:00:00 --`, uuid.NewString())
+		entity.Email = name
+
+		err = repo.Delete(entity)
+		assert.NotNil(t, err)
+		assert.Equal(t, "no register found", err.Error())
+	})
+
+	t.Run("Connection Closed", func(t *testing.T) {
+		entity := createRegister()
+
+		sqlDB, _ := db.DB()
+		err := sqlDB.Close()
+		assert.Nil(t, err)
+
+		err = repo.Delete(entity)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "sql: database is closed")
+	})
+}
